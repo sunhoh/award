5613362b5c63eed5ba80c839449de1e21e307ef0
sunhoh <dltjsgho94@gmail.com>
2025-01-03 16:18:29 +0900

    123

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      swiper: 'swiper',
-    },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    middlewareMode: true,
   },
 });
